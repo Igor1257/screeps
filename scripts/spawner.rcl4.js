e7dca6810cd7bf5db2908677e8aad6872e1c5e69
Igor Kabantsev <i_kabantsev@wargaming.net>
2018-11-13 15:55:12 +0300

    new body for builder was added

--- scripts/spawner.rcl4.js
+++ scripts/spawner.rcl4.js
@@ -48,9 +48,10 @@ var spawnerRCL4 = function(spawn){
     if (this.spawn.room.memory.extentions.length >= 5) this.creepTypes.miner.body = [WORK,WORK,WORK,WORK,MOVE,MOVE]
     if (this.spawn.room.memory.extentions.length >= 10){
         //800 energy >=
-        this.creepTypes.miner.body = [WORK,WORK,WORK,WORK,WORK,WORK,WORK,MOVE,MOVE]
-        this.creepTypes.upgrader.body = [WORK,WORK,WORK,WORK,MOVE,MOVE,MOVE,MOVE,CARRY,CARRY,CARRY,CARRY]
-        this.creepTypes.harvester.body = [WORK,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY]
+        this.creepTypes.builder.body = [WORK,WORK,WORK, MOVE,MOVE,MOVE,MOVE,MOVE,MOVE, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY]
+        this.creepTypes.miner.body = [WORK,WORK,WORK,WORK,WORK,WORK,WORK, MOVE,MOVE]
+        this.creepTypes.upgrader.body = [WORK,WORK,WORK,WORK, MOVE,MOVE,MOVE,MOVE, CARRY,CARRY,CARRY,CARRY]
+        this.creepTypes.harvester.body = [WORK, MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY]
         
     }
    
